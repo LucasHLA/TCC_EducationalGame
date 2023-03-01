ce92211cabf6e9f5a4064948c31bcf08619e23b3
Lucas <LucashenriqueCNA@gmail.com>
2023-03-01 18:31:12 -0300

    Update 01.03.23
    Sprites, colliders, rigidbody, etc...
    
    Basic movements (Walk and jump).

---
+++ TCC_EducationalGame/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Basic Settings")]
+    private Rigidbody2D rb;
+    private Collider2D col;
+    [SerializeField] private int health;
+    [SerializeField] private float speed;
+
+    [Header("State Machine")]
+    private Animator anim;
+    [SerializeField] private enum State {Idle, Walk, Jump};
+    private State state = State.Idle;
+
+    [Header("Jumping related")]
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private bool isJumping;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+
+        anim.SetInteger("State", (int)state);
+    }
+    private void FixedUpdate()
+    {
+        BaseMovement();
+    }
+
+    private void BaseMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if( horizontal > 0)
+        {
+            state = State.Walk;
+            transform.eulerAngles = new Vector3(0,0,0);
+        }
+        else if( horizontal < 0)
+        {
+            state = State.Walk;
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        
+        if(horizontal == 0)
+        {
+            state = State.Idle;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && col.IsTouchingLayers(ground))
+        {
+            state = State.Jump;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            
+        }
+    }
+}
