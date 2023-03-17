5defd91be5a49d47ff87609bc2ba97056c1ff2cb
Lucas Henrique de Lima Antonio <LucashenriqueCNA@gmail.com>
2023-03-17 20:57:55 -0300

    Update 17.03.23.2
    Nothing that really worked, just changed the scene name still need to figure out how to make the transition smoother.

--- TCC_EducationalGame/Assets/Scripts/SceneController.cs
+++ TCC_EducationalGame/Assets/Scripts/SceneController.cs
@@ -7,16 +7,18 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField] private string sceneName;
     //Make the transition longer and actually create a transition before hte scenes with a nice little music or something that feels nice
-    void LoadScene()
-    {
-        SceneManager.LoadScene(sceneName);
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            LoadScene();
+            StartCoroutine(ClearTransition());
         }
+    }
+
+    IEnumerator ClearTransition()
+    {
+        yield return new WaitForSeconds(1.4f);
+        SceneManager.LoadSceneAsync(sceneName);
     }
 }
