c2ba8813722d085bfb045452d448e84dd9b6012e
Lucas <LucashenriqueCNA@gmail.com>
2023-03-19 23:00:01 -0300

    Update 19.03.23
    Now we have a cool transition, YAY!
    
    Still need o decide if the level name wil be in the game controller script of the transition.
    
    still need to make a poem in between tuutorial and Lab scene.
    
    more to come...

--- TCC_EducationalGame/Assets/Scripts/SceneController.cs
+++ TCC_EducationalGame/Assets/Scripts/SceneController.cs
@@ -6,8 +6,14 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
     [SerializeField] private string sceneName;
+    public GameObject levelName;
     //Make the transition longer and actually create a transition before hte scenes with a nice little music or something that feels nice
 
+
+    private void Update()
+    {
+        StartCoroutine(ShowName());
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -18,7 +24,14 @@ public class SceneController : MonoBehaviour
 
     IEnumerator ClearTransition()
     {
+        GameObject.FindGameObjectWithTag("Transition").GetComponent<Animator>().SetTrigger("end");
         yield return new WaitForSeconds(1.4f);
         SceneManager.LoadSceneAsync(sceneName);
+        
+    }
+    IEnumerator ShowName()
+    {
+        yield return new WaitForSeconds(1.75f);
+        levelName.SetActive(true);
     }
 }
