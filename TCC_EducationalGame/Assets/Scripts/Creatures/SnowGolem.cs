1fdd783552436b1228a44920343951a9c77d523a
Lucas Henrique de Lima Antonio <LucashenriqueCNA@gmail.com>
2023-05-10 22:06:02 -0300

    Update 10.05.23.2
    Improved the Snow Golem.
    
    Now it throws snow ball once at a time, but still need to make this logic better.
    
    still need the same boss camera as in the autumn level.
    
    more to come...

--- TCC_EducationalGame/Assets/Scripts/Creatures/SnowGolem.cs
+++ TCC_EducationalGame/Assets/Scripts/Creatures/SnowGolem.cs
@@ -10,6 +10,21 @@ public class SnowGolem : MonoBehaviour
 
     [Header("Attack Related")]
     public Transform firePoint;
+    public Transform robot;
+    public GameObject snowBall;
+
+    [Header("Golem Area")]
+    public LayerMask layer;
+    public Vector3 offset;
+    public float range;
+
+    [Header("Shooting related")]
+    [SerializeField] private bool isShooting;
+    public float throwForce;
+    public int maxThrows = 3;
+    public float throwInterval = 2f;
+
+    private int throwCount = 0;
 
     private void Awake()
     {
@@ -18,18 +33,23 @@ public class SnowGolem : MonoBehaviour
 
     void Update()
     {
-        if(health <= 0)
+        if (health <= 0)
         {
             Defeated();
         }
     }
 
+    private void FixedUpdate()
+    {
+        WakeUpArea();
+    }
+
     public void Damage()
     {
-        if(health > 0)
+        if (health > 0)
         {
             anim.SetTrigger("hurt");
-            health--; 
+            health--;
         }
     }
 
@@ -37,5 +57,63 @@ public class SnowGolem : MonoBehaviour
     {
         anim.SetTrigger("defeated");
         Destroy(this.gameObject, 0.8f);
+    }
+
+    private void WakeUpArea()
+    {
+        Vector3 pos = transform.position;
+
+        pos += transform.right * offset.x;
+        pos += transform.up * offset.y;
+
+        Collider2D area = Physics2D.OverlapCircle(pos, range, layer);
+        float distance = Vector2.Distance(robot.position, transform.position);
+        if (area != null)
+        {
+            if (area.CompareTag("Robot"))
+            {
+                Debug.Log(distance);
+                if (distance > 10.9)
+                {
+                    anim.SetInteger("state", 1);
+
+                    if (!isShooting)
+                    {
+                        StartCoroutine(ThrowSnowballs());
+                    }
+                }
+            }
+        }
+    }
+
+    private IEnumerator ThrowSnowballs()
+    {
+        isShooting = true;
+        throwCount = 0;
+
+        while (throwCount < maxThrows)
+        {
+            ThrowSnowball();
+            throwCount++;
+            yield return new WaitForSeconds(throwInterval);
+        }
+
+        isShooting = false;
+    }
+
+    private void ThrowSnowball()
+    {
+        GameObject newSnowball = Instantiate(snowBall, firePoint.position, Quaternion.identity);
+        Vector2 direction = (robot.position - firePoint.position).normalized;
+        newSnowball.GetComponent<Rigidbody2D>().AddForce(direction * throwForce, ForceMode2D.Impulse);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * offset.x;
+        pos += transform.up * offset.y;
+
+        Gizmos.DrawWireSphere(pos, range);
     }
 }
--- TCC_EducationalGame/Assets/Scripts/Robots/TankerRobot.cs
+++ TCC_EducationalGame/Assets/Scripts/Robots/TankerRobot.cs
@@ -19,6 +19,8 @@ public class TankerRobot : Robot
     public Vector3 respawnPoint;
     private  SpriteRenderer sr;
 
+
+
     [Header("Flash related")]
     private bool isFlashing = false;
 
