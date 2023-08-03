3c7dd5c858b37d24ce5a8b8e062c805c983625c8
Lucas Henrique de Lima Antonio <LucashenriqueCNA@gmail.com>
2023-08-03 19:49:52 -0300

    Update 03.08.23
    Info scenes in full screen bug fixed.
    
    God is good.

--- TCC_EducationalGame/Assets/Scripts/InfoScenesScripts/InformationController.cs
+++ TCC_EducationalGame/Assets/Scripts/InfoScenesScripts/InformationController.cs
@@ -7,12 +7,14 @@ public class InformationController : MonoBehaviour
     public GameObject continueButton;
     public GameObject levelObject;
     public GameObject target;
+    public GameObject teleporter;
 
     public void Continue()
     {
         continueButton.SetActive(false);
         levelObject.SetActive(false);
         GameObject.FindObjectOfType<InformationText>().textComponent.text = string.Empty;
-        target.transform.position = new Vector3(+690,transform.position.y,transform.position.z);
+        target.transform.position = new Vector3(+1500,transform.position.y,transform.position.z);
+        teleporter.transform.position = new Vector3(+1410, transform.position.y, transform.position.z);
     }
 }
